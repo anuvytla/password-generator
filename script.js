8f706d480c5f26a7af99d843b0e4a38e51d5e2ba
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-05-11 22:54:53 -0700

    added user input prompts for password

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+var charLength;
+var newArr = [];
+
+var specialChar = ["+", "-", "&", "!", "(", ")", "{", "}", "[", "]", "^",
+"~", "*", "?", ":","/","#","@","$","%"];
+var upperCases =['A','B', 'C', 'D','E', 'F', 'G', 'H','I', 'J', 'K', 'L', 'M', 'N','O', 'P', 'Q', 'R', 'S', 'T', 'U','V','W', 'X','Y', 'Z'];
+var lowerCases=['a','b', 'c', 'd','e', 'f', 'g', 'h','i', 'j', 'k', 'l', 'm', 'n','o', 'p', 'q', 'r', 's', 't', 'u','v','w', 'x','y', 'z'];
+var nums=['1','2','3','4','5','6','7','8','9','0'];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+
+    var pagePrompts = getPrompts();
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -12,3 +23,28 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function getPrompts() {
+    newArr = [];
+
+    charLength = parseInt(prompt("How many characters you want in your password to be? (8 - 128 characters"));
+
+    if (isNaN(charLength) || charLength<8 ||charLength>128) {
+        alert('characters has to be numbers between 8-128 digits. Please try again');
+        return false;
+    }
+    if (confirm('Would you like specialcharacter letters to be in your password')){
+        newArr=newArr.concat(specialChar);
+    }
+    if (confirm('Would you like uppercase letters to be in your password')) {
+        newArr=newArr.concat(upperCases);
+    }
+    if (confirm('Would you like lowercase letters to be in your password')) {
+        newArr=newArr.concat(lowerCases);
+    }
+    if (confirm('Would you like Numbers to be in your password')){
+        newArr=newArr.concat(nums);
+    }
+    return true;
+
+}
